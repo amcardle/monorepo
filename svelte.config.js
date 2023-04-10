2bd02b471db68301aa9dd76da8c2e6224267f193
amcardle <38906087+amcardle@users.noreply.github.com>
2023-04-10 13:22:10 +0000

    alternative SPA fallback

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '200.html', // may differ from host to host
-			precompress: false,
-			strict: true
+			fallback: 'index.html', // may differ from host to host
 		  })
 	}
 };
