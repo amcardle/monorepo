3c60154f1d9294b6dfce95b1aacd5677431d7371
amcardle <38906087+amcardle@users.noreply.github.com>
2023-04-12 10:35:33 +0000

    auto update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,28 @@
 <script>
   import { onMount } from 'svelte'
   import { pwaInfo } from 'virtual:pwa-info'
-
-  let ReloadPrompt
+  
   onMount(async () => {
-    pwaInfo && (ReloadPrompt = (await import('$lib/ReloadPrompt.svelte')).default)
+    if (pwaInfo) {
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({
+        immediate: true,
+        onRegistered(r) {
+          // uncomment following code if you want check for updates
+          // r && setInterval(() => {
+          //    console.log('Checking for sw update')
+          //    r.update()
+          // }, 20000 /* 20s for testing purposes */)
+          console.log(`SW Registered: ${r}`)
+        },
+        onRegisterError(error) {
+          console.log('SW registration error', error)
+        }
+      })
+    }
   })
-
-  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''  
+  
+  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''
 </script>
 
 <svelte:head>
@@ -18,7 +33,3 @@
 <main>
   <slot />
 </main>
-
-{#if ReloadPrompt}
-  <svelte:component this={ReloadPrompt} />
-{/if}
