2df13156fb55445a889481c9449fe579ca5e3b9e
amcardle <38906087+amcardle@users.noreply.github.com>
2023-04-12 10:27:50 +0000

    update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,33 @@ import { defineConfig } from 'vitest/config';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA()],
+	plugins: [sveltekit(), SvelteKitPWA({manifest: {
+		short_name: 'SvelteKit PWA',
+		name: 'SvelteKit PWA',
+		start_url: '/',
+		scope: '/',
+		display: 'standalone',
+		theme_color: "#ffffff",
+		background_color: "#ffffff",
+		icons: [
+			{
+				src: '/pwa-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+			},
+			{
+				src: '/pwa-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+			},
+			{
+				src: '/pwa-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+				purpose: 'any maskable',
+			},
+		],
+	},})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
