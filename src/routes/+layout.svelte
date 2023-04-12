ea89ca119f3bfdaeb4b5bd0a22072d8ae29a64c4
amcardle <38906087+amcardle@users.noreply.github.com>
2023-04-12 10:19:28 +0000

    trying to make pwa work

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount } from 'svelte'
+  import { pwaInfo } from 'virtual:pwa-info'
+
+  let ReloadPrompt
+  onMount(async () => {
+    pwaInfo && (ReloadPrompt = (await import('$lib/ReloadPrompt.svelte')).default)
+  })
+
+  $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : ''  
+</script>
+
+<svelte:head>
+    {@html webManifest}
+</svelte:head>
+
+<main>
+  <slot />
+  does this work?
+</main>
+
+{#if ReloadPrompt}
+  <svelte:component this={ReloadPrompt} />
+{/if}
