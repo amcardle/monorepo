75be15a3515b4d655de9789f98bbedee46c390e0
amcardle <38906087+amcardle@users.noreply.github.com>
2023-04-12 10:49:54 +0000

    change caching

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,41 @@ import { defineConfig } from 'vitest/config';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 export default defineConfig({
-	plugins: [sveltekit(), SvelteKitPWA({manifest: {
-		short_name: 'SvelteKit PWA',
-		name: 'SvelteKit PWA',
-		start_url: '/',
-		scope: '/',
-		display: 'standalone',
-		theme_color: "#ffffff",
-		background_color: "#ffffff",
-		icons: [
-			{
-				src: '/pwa-192x192.png',
-				sizes: '192x192',
-				type: 'image/png',
-			},
-			{
-				src: '/pwa-512x512.png',
-				sizes: '512x512',
-				type: 'image/png',
-			},
-			{
-				src: '/pwa-512x512.png',
-				sizes: '512x512',
-				type: 'image/png',
-				purpose: 'any maskable',
-			},
-		],
-	},})],
+	plugins: [sveltekit(), SvelteKitPWA({
+		injectManifest: {
+			globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+		},
+		workbox: {
+			globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+		},
+		manifest: {
+			short_name: 'SvelteKit PWA',
+			name: 'SvelteKit PWA',
+			start_url: '/',
+			scope: '/',
+			display: 'standalone',
+			theme_color: "#ffffff",
+			background_color: "#ffffff",
+			icons: [
+				{
+					src: '/pwa-192x192.png',
+					sizes: '192x192',
+					type: 'image/png',
+				},
+				{
+					src: '/pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+				},
+				{
+					src: '/pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'any maskable',
+				},
+			],
+		},
+	})],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
